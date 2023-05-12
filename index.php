45d502dc1fa607a8bd0e5f9e09d1412c358a07c0
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-12 16:23:06 +0200

    password generate

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+
+//verifico se esiste il parametro 
+if(isset($_GET['password-length'])){
+    //var_dump($_GET['password-length']);
+
+$password_lenght =$_GET['password-length'];
+
+
+//array dei caratteri che si inseriranno nella password generata
+$lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+$uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+$numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+$special_characters = ["!","@", "#", "$", "%", "^", "&", "*", "-", "+", "/", "?"];
+
+//unisco gli array
+ $range_characters = array_merge($lowercase, $uppercase, $numbers, $special_characters);
+
+ $password = '';
+//ciclo for per la generazione della password
+for($i = 1; $i <= $password_lenght; $i ++){
+    
+    // per ogni ciclo prendo un numero randomico tra 0 e lunghezza range_characrter
+    $random = rand(0, count( $range_characters)-1);
+    
+    //concateno il carattere preso in posizione random 
+    $password .= $range_characters[$random];
+
+
+}
+
+var_dump($password);
+
+
+ //var_dump($range_characters);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,10 +53,10 @@
     <div class="container">
         <h1>Strong Password Generator</h1>
         <div class="container">
-            <form>
+            <form action="index.php" method="GET">
                 <div class="mb-3">
                     <label for="password-length" class="form-label">Your password length</label>
-                    <input type="number" class="form-control" id="password-length" min="8" max="30">
+                    <input type="number" name="password-length" class="form-control" id="password-length" min="8" max="30">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
